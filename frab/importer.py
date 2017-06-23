8398b86abc89b53eb08e9c9d74bfbbc848ce71ac
bitofhope <bitofhope@kapsi.fi>
2017-06-24 01:48:03 +0300

    Make frab/importer.py use JSON instead of XML.

--- frab/importer.py
+++ frab/importer.py
@@ -9,6 +9,7 @@ from datetime import timedelta
 
 import dateutil.parser
 import defusedxml.ElementTree as ET
+import json
 
 def cleanhtml(raw_html):
     cleanr = re.compile('<.*?>')
@@ -16,42 +17,25 @@ def cleanhtml(raw_html):
     return cleantext
 
 def get_schedule(url, group):
-    def load_events(xml):
+    
+    def load_events(js):
         def to_unixtimestamp(dt):
             dt = dt.astimezone(pytz.utc)
             ts = int(calendar.timegm(dt.timetuple()))
             return ts
-        def text_or_empty(node, child_name):
-            child = node.find(child_name)
-            if child is None:
-                return u""
-            if child.text is None:
-                return u""
-            return unicode(child.text)
         def parse_duration(value):
             h, m = map(int, value.split(':'))
             return timedelta(hours=h, minutes=m)
 
-        def all_events():
-            schedule = ET.fromstring(xml)
-            for day in schedule.findall('day'):
-                for room in day.findall('room'):
-                    for event in room.findall('event'):
-                        yield event
-
         parsed_events = []
-        for event in all_events():
-            start = dateutil.parser.parse(event.find('date').text)
-            duration = parse_duration(event.find('duration').text)
+        for event in json.loads(js):
+            start = dateutil.parser.parse(event["start_time"])
+            duration = event["length"]
             end = start + duration
 
             persons = event.find('persons')
             if persons is not None:
                 persons = persons.findall('person')
-
-            links = event.find('links')
-            if links is not None:
-                links = links.findall('link')
 
             parsed_events.append(dict(
                 start = start.astimezone(pytz.utc),
@@ -60,16 +44,11 @@ def get_schedule(url, group):
                 start_unix  = to_unixtimestamp(start),
                 end_unix = to_unixtimestamp(end),
                 duration = int(duration.total_seconds() / 60),
-                title = text_or_empty(event, 'title'),
-                place = text_or_empty(event, 'room'),
-                abstract = cleanhtml(text_or_empty(event, 'abstract')),
-                speakers = [
-                    unicode(person.text.strip()) 
-                    for person in persons
-                ] if persons else [],
-                link = unicode(links[0].attrib.get('href', '')) if links else '',
-                lang = text_or_empty(event, 'language') or "unk",
-                id = event.attrib["id"],
+                title = event["title"]
+                place = event["room_name"]
+                abstract = cleanhtml(event["description"]),
+                speakers = event["formatted_hosts"]
+                # lang = text_or_empty(event, 'language') or "unk",
                 group = group,
             ))
         return parsed_events
